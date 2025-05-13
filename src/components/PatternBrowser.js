5b0adda8f33b69f307725e18a1c774f24de6a95a
Jason Crist <jcrist@pbking.com>
2025-05-13 15:17:54 -0400

    Improved pattern searching and filtering

--- src/components/PatternBrowser.js
+++ src/components/PatternBrowser.js
@@ -38,7 +38,7 @@ export const PatternGrid = ({ onPatternClick, patterns, editorSettings }) => {
  */
 export const PatternBrowser = ({ onPatternClick }) => {
     const [patterns, setPatterns] = useState([]);
-    const [filteredPatterns, setFilteredPatterns] = useState([]);
+    const [filteredPatterns, setFilteredPatterns] = useState(null);
     const [editorSettings, setEditorSettings] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -47,7 +47,7 @@ export const PatternBrowser = ({ onPatternClick }) => {
         getAllPatterns()
             .then((patterns) => {
                 setPatterns(patterns);
-                setFilteredPatterns(patterns); // Initialize filtered patterns
+                setFilteredPatterns(null);
                 setIsLoading(false);
             })
             .catch((err) => {
@@ -62,17 +62,39 @@ export const PatternBrowser = ({ onPatternClick }) => {
 
     const handleFilterChange = (filters) => {
 
+		if (filters.keyword.length < 2 && filters.category === 'all' && filters.source === 'all' && filters.synced === 'all') {
+			setFilteredPatterns(null);
+			return;
+		}
+		// Filter patterns based on the keyword
+
         const updatedFilteredPatterns = patterns
-		.filter((pattern) => {
-        	// Filter patterns based on the source
-            if (filters.source === 'all') return true; // Show all patterns
-            return pattern.source === filters.source; // Match the source
-        })
-		.filter((pattern) => {
-			// Filter patterns based on the synced status
-			if (filters.synced === 'all') return true; // Show all patterns
-			return pattern.synced === (filters.synced === 'yes'); // Match the synced status
-		});
+			.filter((pattern) => {
+	        	// Filter patterns based on the source
+	            if (filters.source === 'all') return true; // Show all patterns
+	            return pattern.source === filters.source; // Match the source
+	        })
+			.filter((pattern) => {
+				// Filter patterns based on the synced status
+				if (filters.synced === 'all') return true; // Show all patterns
+				return pattern.synced === (filters.synced === 'yes'); // Match the synced status
+			})
+			.filter((pattern) => {
+				// Filter patterns based on the category
+				if (filters.category === 'all') return true; // Show all patterns
+				return pattern.categories.some((category) => category.slug === filters.category); // Match the category
+			})
+			.filter((pattern) => {
+				// Filter patterns based on the keyword / title
+				if ( ! filters.keyword ) return true;
+				return (
+					// search pattern title
+					pattern.title.toLowerCase().includes(filters.keyword.toLowerCase())
+
+					// search pattern keywords
+					|| (pattern.keywords && pattern.keywords.some((keyword) => keyword.toLowerCase().includes(filters.keyword.toLowerCase())))
+				);
+			});
 
         setFilteredPatterns(updatedFilteredPatterns);
 
@@ -101,11 +123,25 @@ export const PatternBrowser = ({ onPatternClick }) => {
                         onFilterChange={handleFilterChange}
 						onCreatePattern={handleCreatePattern}
                     />
+					{filteredPatterns?.length > 0 && (
 					<PatternGrid
                         onPatternClick={onPatternClick}
                         patterns={filteredPatterns}
                         editorSettings={editorSettings}
                     />
+					)}
+					{filteredPatterns?.length === 0 && (
+						<div>
+							<p>{__('No patterns found', 'pattern-manager')}</p>
+						</div>
+
+					)}
+
+					{!filteredPatterns && (
+						<div>
+							<p>{__('Welcome to the Pattern Manager.  Start searching to find a pattern to edit or create a new one.', 'pattern-manager')}</p>
+						</div>
+					)}
                 </>
             )}
         </div>
--- src/components/PatternBrowserPanel.js
+++ src/components/PatternBrowserPanel.js
@@ -1,12 +1,17 @@
 import { __ } from '@wordpress/i18n';
 import { PanelBody, SelectControl, TextControl, TextareaControl, CheckboxControl, Button } from '@wordpress/components';
 import { useState } from '@wordpress/element';
+import {
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+  } from '@wordpress/components';
 
 /**
  * PatternBrowserPanel Component
  * Displays a sidebar for filters or additional controls.
  */
 export const PatternBrowserPanel = ({ editorSettings, patterns, onFilterChange, onCreatePattern }) => {
+
     const [filterOptions, setFilterOptions] = useState({
         source: 'all',
         synced: 'all',
@@ -39,68 +44,57 @@ export const PatternBrowserPanel = ({ editorSettings, patterns, onFilterChange, 
         });
     };
 
+	const patternCategories = Object.values(patterns.reduce((acc, pattern) => {
+		pattern.categories.forEach((category) => {
+			if (!acc[category.slug]) {
+				acc[category.slug] = {
+					label: category.name,
+					value: category.slug,
+				};
+			}
+		});
+		return acc;
+	}, {'all': { label: __('All', 'pattern-manager'), value: 'all' } }));
+
     return (
         <div className="pattern-manager__sidebar">
 
-                <SelectControl
-                    label={__('Source', 'pattern-manager')}
-                    value={filterOptions.source}
-                    options={[
-                        { label: __('All', 'pattern-manager'), value: 'all' },
-                        { label: __('User', 'pattern-manager'), value: 'user' },
-                        { label: __('Theme', 'pattern-manager'), value: 'theme' },
-                        { label: __('Core', 'pattern-manager'), value: 'core' },
-                    ]}
-                    onChange={(value) => updateFilterOptions('source', value)}
-                />
-
-                <SelectControl
-                    label={__('Synced', 'pattern-manager')}
-                    value={filterOptions.synced}
-                    options={[
-                        { label: __('All', 'pattern-manager'), value: 'all' },
-                        { label: __('Synced', 'pattern-manager'), value: 'yes' },
-                        { label: __('Unsynced', 'pattern-manager'), value: 'no' },
-                    ]}
-                    onChange={(value) => updateFilterOptions('synced', value)}
-                />
-
-                {/* Categories Filter */}
-                <SelectControl
-                    label={__('Categories', 'pattern-manager')}
-                    value={filterOptions.category}
-                    options={[
-                        { label: __('All Categories', 'pattern-manager'), value: 'all' },
-                        { label: __('Category 1', 'pattern-manager'), value: 'category1' },
-                        { label: __('Category 2', 'pattern-manager'), value: 'category2' },
-                    ]}
-                    onChange={(value) => updateFilterOptions('category', value)}
-                />
-
-                {/* Keyword Filter */}
-                <TextControl
-                    label={__('Keyword', 'pattern-manager')}
+				<TextControl
+                    label={__('Keyword / Title', 'pattern-manager')}
                     value={filterOptions.keyword}
-                    placeholder={__('Search by keyword...', 'pattern-manager')}
+                    placeholder={__('Search...', 'pattern-manager')}
                     onChange={(value) => updateFilterOptions('keyword', value)}
                 />
 
-				<hr />
+				<ToggleGroupControl
+					isBlock
+                    label={__('Source', 'pattern-manager')}
+                    value={filterOptions.source}
+                    onChange={(value) => updateFilterOptions('source', value)}
+				>
+					<ToggleGroupControlOption label={__('All', 'pattern-manager')} value="all" />
+					<ToggleGroupControlOption label={__('User', 'pattern-manager')} value="user" />
+					<ToggleGroupControlOption label={__('Theme', 'pattern-manager')} value="theme" />
+				</ToggleGroupControl>
 
-                <TextControl
-                    label={__('Name', 'pattern-manager')}
-                    value={newPattern.title}
-                    placeholder={__('Enter pattern name...', 'pattern-manager')}
-                    onChange={(value) => updateNewPattern('title', value)}
+				<ToggleGroupControl
+					isBlock
+                    label={__('Synced', 'pattern-manager')}
+                    value={filterOptions.synced}
+                    onChange={(value) => updateFilterOptions('synced', value)}
+				>
+					<ToggleGroupControlOption label={__('All', 'pattern-manager')} value="all" />
+					<ToggleGroupControlOption label={__('Synced', 'pattern-manager')} value="yes" />
+					<ToggleGroupControlOption label={__('Unsynced', 'pattern-manager')} value="no" />
+				</ToggleGroupControl>
+
+                <SelectControl
+                    label={__('Category', 'pattern-manager')}
+                    value={filterOptions.category}
+                    options={patternCategories}
+                    onChange={(value) => updateFilterOptions('category', value)}
                 />
-                <Button
-		    		variant="primary"
-                    onClick={handleCreatePattern}
-                    disabled={!newPattern.title}
-					style={{ width: '100%' }}
-                >
-                    {__('Create Pattern', 'pattern-manager')}
-                </Button>
+
         </div>
     );
 };
