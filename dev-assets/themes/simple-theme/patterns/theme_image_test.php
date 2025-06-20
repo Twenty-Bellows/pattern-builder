55ae11a0bd1dffcd621a64ff8c4fc50c99e78a78
Jason Crist <jcrist@pbking.com>
2025-06-20 06:34:25 -0400

    Add image pattern

---
+++ dev-assets/themes/simple-theme/patterns/theme_image_test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Theme Image Test
+ * Slug: simple-theme/theme-image-test
+ * Description:
+ * Synced: yes
+ * Categories: media
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading">Image Block</h2>
+<!-- /wp:heading -->
+
+<!-- wp:image {"id":1309,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/twenty_bellows_logo.png'; ?>" alt="" class="wp-image-1309"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
