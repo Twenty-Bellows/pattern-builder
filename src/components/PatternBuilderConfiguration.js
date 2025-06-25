dfd8e1b4aa065cd7a712f5471f4cfee6ce5c0cb4
Jason Crist <jcrist@pbking.com>
2025-06-25 14:21:13 -0400

    Add new configuration panel

---
+++ src/components/PatternBuilderConfiguration.js
@@ -0,0 +1,16 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalVStack as VStack,
+} from '@wordpress/components';
+
+export const PatternBuilderConfiguration = () => {
+	return (
+		<VStack spacing={ 4 }>
+			{/* Configuration content will be added here */}
+		</VStack>
+	);
+};
