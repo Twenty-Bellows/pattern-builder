cdb0a6f4f9a7a26b3c5708a26dd0622c944316de
Jason Crist <jcrist@pbking.com>
2025-06-24 23:37:53 -0400

    Format utilties and configuration files

--- scripts/version-bump.js
+++ scripts/version-bump.js
@@ -1,47 +1,50 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+const fs = require( 'fs' );
+const path = require( 'path' );
 
 // Get the current version from package.json
-const packageJsonPath = path.join(__dirname, '..', 'package.json');
-const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+const packageJsonPath = path.join( __dirname, '..', 'package.json' );
+const packageJson = JSON.parse( fs.readFileSync( packageJsonPath, 'utf8' ) );
 const currentVersion = packageJson.version;
 
 // Parse version components
-const versionParts = currentVersion.split('.');
-const major = parseInt(versionParts[0]);
-const minor = parseInt(versionParts[1]);
-const patch = parseInt(versionParts[2]);
+const versionParts = currentVersion.split( '.' );
+const major = parseInt( versionParts[ 0 ] );
+const minor = parseInt( versionParts[ 1 ] );
+const patch = parseInt( versionParts[ 2 ] );
 
 // Increment patch version
-const newVersion = `${major}.${minor}.${patch + 1}`;
+const newVersion = `${ major }.${ minor }.${ patch + 1 }`;
 
-console.log(`Bumping version from ${currentVersion} to ${newVersion}`);
+console.log( `Bumping version from ${ currentVersion } to ${ newVersion }` );
 
 // Update package.json
 packageJson.version = newVersion;
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, '\t') + '\n');
-console.log('✓ Updated package.json');
+fs.writeFileSync(
+	packageJsonPath,
+	JSON.stringify( packageJson, null, '\t' ) + '\n'
+);
+console.log( '✓ Updated package.json' );
 
 // Update readme.txt
-const readmePath = path.join(__dirname, '..', 'readme.txt');
-let readmeContent = fs.readFileSync(readmePath, 'utf8');
+const readmePath = path.join( __dirname, '..', 'readme.txt' );
+let readmeContent = fs.readFileSync( readmePath, 'utf8' );
 readmeContent = readmeContent.replace(
-    /^Stable tag: .+$/m,
-    `Stable tag: ${newVersion}`
+	/^Stable tag: .+$/m,
+	`Stable tag: ${ newVersion }`
 );
-fs.writeFileSync(readmePath, readmeContent);
-console.log('✓ Updated readme.txt');
+fs.writeFileSync( readmePath, readmeContent );
+console.log( '✓ Updated readme.txt' );
 
 // Update pattern-builder.php
-const pluginPath = path.join(__dirname, '..', 'pattern-builder.php');
-let pluginContent = fs.readFileSync(pluginPath, 'utf8');
+const pluginPath = path.join( __dirname, '..', 'pattern-builder.php' );
+let pluginContent = fs.readFileSync( pluginPath, 'utf8' );
 pluginContent = pluginContent.replace(
-    /^\s*\*\s*Version:\s*.+$/m,
-    ` * Version: ${newVersion}`
+	/^\s*\*\s*Version:\s*.+$/m,
+	` * Version: ${ newVersion }`
 );
-fs.writeFileSync(pluginPath, pluginContent);
-console.log('✓ Updated pattern-builder.php');
+fs.writeFileSync( pluginPath, pluginContent );
+console.log( '✓ Updated pattern-builder.php' );
 
-console.log(`\nVersion bump complete! New version: ${newVersion}`);
+console.log( `\nVersion bump complete! New version: ${ newVersion }` );
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
 
 defaultConfig[ 0 ] = {
 	...defaultConfig[ 0 ],
-	plugins: [
-		...defaultConfig[0].plugins,
-		new CleanWebpackPlugin(),
-	],
+	plugins: [ ...defaultConfig[ 0 ].plugins, new CleanWebpackPlugin() ],
 	...{
 		entry: {
-			'PatternBuilder_EditorTools': './src/PatternBuilder_EditorTools.js',
-			'PatternBuilder_Admin': './src/PatternBuilder_Admin.js',
+			PatternBuilder_EditorTools: './src/PatternBuilder_EditorTools.js',
+			PatternBuilder_Admin: './src/PatternBuilder_Admin.js',
 		},
 	},
 };
